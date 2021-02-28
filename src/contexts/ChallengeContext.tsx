923054191c9c5ffa3696989bcb7e0eff779d628b
Viserion Notebook <matheustae@hotmail.com>
2021-02-28 07:26:05 -0300

    Nlw

---
+++ src/contexts/ChallengeContext.tsx
@@ -0,0 +1,68 @@
+import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json'
+
+interface Challenge {
+    type: 'body' | 'eye';
+    description: string;
+    amount: number;
+}
+
+interface ChallengeContextData {
+    level: number;
+    currentExperience: number;
+    experienceToNextLevel: number;
+    challengesCompleted:number;
+    activeChallenge: Challenge;
+    levelUp: () => void;
+    startNewChallenge: () => void;
+    resetChallenge: () => void;
+}
+
+interface ChallengeProviderProps {
+    children: ReactNode;
+}
+
+export const ChallengeContext = createContext({} as ChallengeContextData);
+
+
+export function ChallengeProvider({ children }: ChallengeProviderProps) {
+    const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(30);
+    const [challengesCompleted, setChallengesCompleted] = useState(3);
+
+    const [activeChallenge, setActiveChallenge] = useState(null);
+
+    const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
+
+    function levelUp() {
+        setLevel(level + 1);
+    }
+
+    function startNewChallenge() {
+        const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallengeIndex];
+
+        setActiveChallenge(challenge);
+    }
+
+    function resetChallenge() {
+        setActiveChallenge(null);
+    }
+
+    return (
+        <ChallengeContext.Provider
+            value={{
+                level,
+                currentExperience,
+                experienceToNextLevel,
+                challengesCompleted,
+                levelUp,
+                startNewChallenge,
+                activeChallenge,
+                resetChallenge
+            }}
+        >
+            {children}
+        </ChallengeContext.Provider>
+    )
+}
---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+
+import '../styles/global.css'
+
+import { ChallengeProvider } from '../contexts/ChallengeContext';
+
+
+function MyApp({ Component, pageProps }) {
+    return (
+
+        <ChallengeProvider>
+
+            <Component {...pageProps} />
+
+        </ChallengeProvider>
+
+    )
+}
+
+export default MyApp
